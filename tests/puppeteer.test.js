7b2ac38d94fefa96fd143827de3e9b6112c8d41b
Hady Osman <hady.osman@xero.com>
2020-01-03 19:20:13 +1300

    Add test to scrape long highlights with "more" link

--- tests/puppeteer.test.js
+++ tests/puppeteer.test.js
@@ -14,6 +14,7 @@ describe("Scrape kindle highlights for a given book", () => {
     await loadHtmlFromFile("./tests/assets/fragment.highlight.html");
 
     const highlights = await scraper.scrapeHighlightsFromPageUrl();
+
     expect(highlights).toHaveLength(1);
     expect(highlights[0]).toEqual({
       annotationId: "aJ2HUN17A79BA%7C-0-%7C",
@@ -22,6 +23,21 @@ describe("Scrape kindle highlights for a given book", () => {
       locationPercentage: "8%",
       text:
         "What is this force that is controlling you even now and will continue to do so for the rest of your life? PAIN and PLEASURE! Everything you and I do, we do either out of our need to avoid pain or our desire to gain pleasure."
+    });
+  });
+
+  it("kindle highlight with more link is scraped accurately", async () => {
+    await loadHtmlFromFile("./tests/assets/fragment.highlight-more.html");
+
+    const highlights = await scraper.scrapeHighlightsFromPageUrl();
+    expect(highlights).toHaveLength(1);
+    expect(highlights[0]).toEqual({
+      annotationId: "a1JIMM1S64WZYN%7C-0-%7C",
+      bookId: "6587033",
+      hastNote: false,
+      locationPercentage: "7%",
+      text:
+        "2. Realize that the hardest step in achieving anything is making a true commitment—a true decision. Carrying out your commitment is often much easier than the decision itself, so make your decisions intelligently, but make them quickly. Don’t labor forever over the question of how or if you can do it. Studies have shown that the most successful people make decisions rapidly because they are clear on their values and what they really want for their lives. The same studies show that they are slow to change their decisions, if at all. On the other hand, people who fail usually make decisions slowly and change their minds quickly, always bouncing back and forth. Just decide!"
     });
   });
 });
