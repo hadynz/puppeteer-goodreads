5a6dfbc1c02068e423e18a8e25fb25f141a397a5
Hady Osman <hady.osman@xero.com>
2020-01-05 21:08:09 +1300

    Remove redundant naming of a default export

--- __tests__/util/setPageContentFromFile.ts
+++ __tests__/util/setPageContentFromFile.ts
@@ -1,8 +1,6 @@
 import * as fs from 'fs';
 
-export default async function setPageContentFromFile(
-  fileName: string,
-): Promise<void> {
+export default async function(fileName: string): Promise<void> {
   const html = fs.readFileSync(fileName, 'utf8');
   await page.setContent(html, {
     waitUntil: 'networkidle0',
